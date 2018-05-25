f380f9a342e11b17ebc699ba1ee55dda7496fbd2
Devops999 <palvisharajput21@gmail.com>
2018-05-25 09:56:17 +0530

    adding report in stock app

--- stock/modules/web/src/com/company/stock/web/ext4/purchaseorder/PurchaseBrowseCustomers.java
+++ stock/modules/web/src/com/company/stock/web/ext4/purchaseorder/PurchaseBrowseCustomers.java
@@ -3,16 +3,31 @@ package com.company.stock.web.ext4.purchaseorder;
 import com.company.stock.entity.PurchaseOrder;
 import com.company.stock.service.PurchaseOrderService;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.reports.gui.actions.TablePrintFormAction;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 public class PurchaseBrowseCustomers extends AbstractLookup {
- /*@Inject
-   private PurchaseOrderService orderService;
-     @Override
-   protected boolean preCommit() {
-       PurchaseOrder order = getItem();
-       order.setTax(orderService.AmountAfterTax(order));
-       return super.preCommit();
-   }*/
+    @Inject
+    private Button printDetails;
+    @Inject
+    private GroupTable<PurchaseOrder> purchaseOrdersTable;
+
+    /*@Inject
+       private PurchaseOrderService orderService;
+         @Override
+       protected boolean preCommit() {
+           PurchaseOrder order = getItem();
+           order.setTax(orderService.AmountAfterTax(order));
+           return super.preCommit();
+       }*/
+ @Override
+ public void init(Map<String, Object> params) {
+     TablePrintFormAction action = new TablePrintFormAction("report", purchaseOrdersTable);
+     purchaseOrdersTable.addAction(action);
+     printDetails.setAction(action);
+ }
 }
